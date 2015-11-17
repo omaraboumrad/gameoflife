3fc83578c4eccc8a21aca7c801a55229b6b5c7a9
Omar Abou Mrad <omar.aboumrad@gmail.com>
2015-11-17 23:29:16 +0200

    cleanups

--- life.py
+++ life.py
@@ -1,10 +1,12 @@
 class Life(object):
+
     def next_gen(self, array):
         new_gen = []
-        for j,row in enumerate(array):
+        for j, row in enumerate(array):
             new_gen_row = []
-            for i,column in enumerate(row):
-                successor = self.get_successor(array, j, i, len(array), len(row))
+            for i, column in enumerate(row):
+                successor = self.get_successor(array, j, i,
+                                               len(array), len(row))
                 new_gen_row.append(successor)
             new_gen.append(new_gen_row)
         return new_gen
@@ -14,12 +16,9 @@ class Life(object):
         lmi = lambda x: 0 if x < 0 else x
         lma = lambda x, m: m-1 if x == m else x
         living_cells = sum((
-            sum(array[lmi(j-1)   ][lmi(i-1):lma(i+2, w)]),
-            sum(array[j          ][lmi(i-1):lma(i+2, w)]),
-            sum(array[lma(j+1, h)][lmi(i-1):lma(i+2, w)]))) - alive
+            sum(array[lmi(j - 1)][lmi(i - 1):lma(i + 2, w)]),
+            sum(array[j][lmi(i - 1):lma(i + 2, w)]),
+            sum(array[lma(j + 1, h)][lmi(i - 1):lma(i + 2, w)]))) - alive
 
-        return 1 if (alive and living_cells in (2,3)) or \
-                    (not alive and living_cells == 3) else 0
-
-
-          
+        return 1 if (alive and (living_cells in (2, 3))) or \
+            (not alive and living_cells == 3) else 0
